86760af10cfa73a1b11531919011b8d0afcd825d
Shreyans <78007563+sdey02@users.noreply.github.com>
2023-11-06 15:58:51 -0500

    SDL Initialization

--- main.cpp
+++ main.cpp
@@ -6,42 +6,47 @@
 
 typedef struct {
     SDL_Window *window;
+    SDL_Renderer *renderer;
 } sdl_t;
 
 bool init_sdl(sdl_t *sdl){
-    if(SDL_InitSubSystem(SDL_INIT_TIMER || SDL_INIT_AUDIO || SDL_INIT_VIDEO) != 0){
-        SDL_Log("Could not initialize SDL: s\n", SDL_GetError());
-        return false; //SDL init fails
+    if(SDL_InitSubSystem(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0){ //Checks weather SDL Init works
+        SDL_Log("Could not initialize SDL: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
     }
 
-    sdl -> window = SDL_CreateWindow("CHIP 8 Emulator", 640, 480, SDL_WINDOW_RESIZABLE);
+    sdl ->window = SDL_CreateWindow("CHIP 8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE); //Initializes the window
 
-    if(!SDL_CreateWindow()){
-        SDL_Log("Could not initialize window: s\n", SDL_GetError());
-        return false
+    if(!sdl ->window){
+        SDL_Log("Could not initialize window: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
     }
 
-    return true; //SDL init success
+    sdl ->renderer = SDL_CreateRenderer(sdl ->window, -1, SDL_RENDERER_ACCELERATED); //Initializes the render
+
+    if(!sdl ->renderer){
+        SDL_Log("Could not initialize renderer: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;; //SDL init success
 }
 
-void final_cleanup(void){
-    SDL_DestroyWindow(window)
+void final_cleanup(sdl_t *sdl){
+    SDL_DestroyWindow(sdl -> window);
     SDL_Quit(); //Shut Down SDL
 }
 
 int main (int argc, char **argv){
-    std::cout << "Testing" << std::endl;
-
     // Initialize SDL
-    sdl_t sdl {0};
+    sdl_t sdl = {0};
     if (!init_sdl(&sdl)){
-        return 0;
+        exit(EXIT_FAILURE);
     }
 
     // Create Window
 
 
     // Final cleanup
-    final_cleanup();
-
+    final_cleanup(&sdl);
 }
