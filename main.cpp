91d8822d819cd971559d040c8e288e3a56b0abbe
Shreyans <78007563+sdey02@users.noreply.github.com>
2023-11-07 13:00:05 -0500

    Display prints yellow background now.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ typedef struct {
     uint32_t window_height;     // SDL window height
     uint32_t fg_color;          // Foreground color RGBA8888
     uint32_t bg_color;          // Background color RGBA8888
+    uint32_t scale_factor;      // Amount to scale Chip8 pixel by
 } config_t;
 
 bool init_sdl(sdl_t *sdl, config_t config){
@@ -23,7 +24,7 @@ bool init_sdl(sdl_t *sdl, config_t config){
         return false;
     }
 
-    sdl ->window = SDL_CreateWindow("CHIP 8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, config.window_width, config.window_height, SDL_WINDOW_RESIZABLE); //Initializes the window
+    sdl ->window = SDL_CreateWindow("CHIP 8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, config.window_width * config.scale_factor, config.window_height * config.scale_factor, 0); //Initializes the window
 
     if(!sdl ->window){
         SDL_Log("Could not initialize window: %s\n", SDL_GetError());
@@ -47,8 +48,9 @@ bool set_config_from_args(config_t *config, const int argc, char **argv) {
     *config = (config_t){
         .window_width  = 64,    // CHIP8 original X resolution
         .window_height = 32,    // CHIP8 original Y resolution
-        .fg_color = 0xFFFF00FF, // Yellow
-        .bg_color = 0x00000000, // BLACK
+        .fg_color = 0xFFFFFFFF, // WHite
+        .bg_color = 0xFFFF00FF, // Yellow
+        .scale_factor = 20
     };
 
     // Override defaults from passed in arguments
@@ -65,10 +67,19 @@ void final_cleanup(sdl_t *sdl){
     SDL_Quit(); //Shut Down SDL
 }
 
-void clear_screen(const config_t config){
-    const uint8_t r = (config.bg_color << 24);
-    // int SDL_SetRenderDrawColor(sdl.renderer, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
-    int SDL_RenderClear(sdl.renderer);
+void clear_screen(const sdl_t sdl, const config_t config){
+    // RGBA is 32 bits
+    const uint8_t r = (config.bg_color >> 24) & 0xFF; //Set the of R
+    const uint8_t g = (config.bg_color >> 16) & 0xFF; //Set the of G
+    const uint8_t b = (config.bg_color >> 8) & 0xFF; //Set the of B
+    const uint8_t a = (config.bg_color >> 0) & 0xFF; //Set the of A
+
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const sdl_t sdl){
+    SDL_RenderPresent(sdl.renderer);
 }
 
 int main (int argc, char **argv){
@@ -85,12 +96,16 @@ int main (int argc, char **argv){
     }
 
     // Intitial screen clear
-    clear_screen(config);
+    clear_screen(sdl, config);
 
     // Main Loop
     while (true){
+        // Delay for 60FPS
+        SDL_Delay(16);
 
-    }
+        // Update Screen
+        update_screen(sdl);
+        }
 
     // Final cleanup
     final_cleanup(&sdl);
